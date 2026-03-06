922b614f24f5092af01ac358d0cd5d20e977107a
sebastianjnuwu <sebastianjnuwu@gmail.com>
2026-03-06 19:27:04 -0300

    feat: add CORS support using rs/cors package and remove custom CORS middleware

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/redis/go-redis/v9 v9.18.0 // indirect
+	github.com/rs/cors v1.11.1 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/sync v0.10.0 // indirect
--- main.go
+++ main.go
@@ -7,30 +7,46 @@ import (
 	"blob/src/routes"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 )
 
 func main() {
-
 	godotenv.Load()
 	database.Redis()
 	database.Postgres()
 
 	mux := http.NewServeMux()
 	limiter := middleware.Variables()
-	handler := middleware.CORSMiddleware(mux)
 	routes.RegisterRoutes(mux, limiter)
 
-	PORT := os.Getenv("BLOB_PORT")
-	if PORT == "" {
-		PORT = "3000"
+	corsOrigins := os.Getenv("BLOB_CORS_ORIGINS")
+	corsOpts := cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowCredentials: true,
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"},
+		AllowedHeaders:   []string{"Authorization", "Content-Type", "Accept", "Origin"},
 	}
-	HOST := os.Getenv("BLOB_HOST")
-	if HOST == "" {
-		HOST = "localhost"
+	if corsOrigins != "*" && corsOrigins != "" {
+		origins := strings.Split(corsOrigins, ",")
+		for i := range origins {
+			origins[i] = strings.TrimSpace(origins[i])
+		}
+		corsOpts.AllowedOrigins = origins
+	}
+	handler := cors.New(corsOpts).Handler(mux)
+
+	port := os.Getenv("BLOB_PORT")
+	if port == "" {
+		port = "3000"
+	}
+	host := os.Getenv("BLOB_HOST")
+	if host == "" {
+		host = "localhost"
 	}
 
-	functions.Info("[SERVER] Server running at: http://%s:%s", HOST, PORT)
-	http.ListenAndServe(HOST+":"+PORT, handler)
+	functions.Info("[SERVER] Server running at: http://%s:%s", host, port)
+	http.ListenAndServe(host+":"+port, handler)
 }
